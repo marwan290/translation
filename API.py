cfd878f22b219ba35707a845aa2ca82fd67cc14f
marwankhaled <MKH@corelia.ai>
2023-06-09 15:49:08 +0300

    Update API.py

--- API.py
+++ API.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify,render_template
 from transformers import MarianTokenizer, MarianMTModel
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from utils import detect_lang,translation
-
-from langdetect import detect
 import unicodedata
+
 
 
 
@@ -27,7 +26,6 @@ ar_en_tokenizer = AutoTokenizer.from_pretrained(ar_en_model_path)
 en_ar_model = AutoModelForSeq2SeqLM.from_pretrained(en_ar_model_path)
 en_ar_tokenizer = AutoTokenizer.from_pretrained(en_ar_model_path)
 
-
 def check_for_language_chars(sentence):
     for char in sentence:
         category = unicodedata.category(char)
@@ -43,26 +41,21 @@ def index():
 def translate_text():
     print(request.form)
     sentence = request.form['text']
+    lang = detect_lang.predict_language(sentence, lang_vectorizer, lang_model)
     word_exist = check_for_language_chars(sentence)
+    
     if word_exist:
-        lang = detect(sentence)
-        
-        if lang == "ar":
-            lang = "Arabic"
+        if lang == "Arabic":
             translated_text = translation.translate(sentence, ar_en_model, ar_en_tokenizer)
-        elif lang == "en":
-            lang = "English"
-            translated_text = translation.translate(sentence, en_ar_model, en_ar_tokenizer)
         else:
-            lang = "Other"
-            translated_text = "Not-Support"
+            translated_text = translation.translate(sentence, en_ar_model, en_ar_tokenizer)
             
     else:
         return "Please Enter Valid Sentence"
 
     # return jsonify({'translation': translated_text})
-    print(translated_text)
-    return  "Input Sentence is :>>> " + sentence+ "\nLanguage is :>>>>  "  + lang + " \n And translation is :>>>>" + str(translated_text)
+    # print(translated_text)
+    return  "Input Sentence is >>> \n" + sentence+ "\nLanguage is :>>>>  "  + lang + " \n And translation is :>>>>" + str(translated_text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
